6dd47f9159a7eff68f8b7de0bbd2771974458bdf
MalenaMoix <moixmalena@gmail.com>
2021-07-25 11:34:58 -0300

    repository initialization

---
+++ src/test/java/dan/jms/dangateway/DanGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package dan.jms.dangateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
