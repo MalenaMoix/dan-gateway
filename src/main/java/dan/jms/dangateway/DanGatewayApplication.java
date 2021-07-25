ee7534e0895f1e31a1b44506a88e997c1a5ab71b
MalenaMoix <moixmalena@gmail.com>
2021-07-25 19:33:34 -0300

    Zuul and Eureka configuration

--- src/main/java/dan/jms/dangateway/DanGatewayApplication.java
+++ src/main/java/dan/jms/dangateway/DanGatewayApplication.java
@@ -2,8 +2,10 @@ package dan.jms.dangateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
 public class DanGatewayApplication {
 
     public static void main(String[] args) {
