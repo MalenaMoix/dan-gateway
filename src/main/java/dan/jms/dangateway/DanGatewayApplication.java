639a8eae2515d47d8b9850bba5ba34c57bd5b208
MalenaMoix <malena.moix@overproofdev.com>
2021-09-21 09:10:05 -0300

    Configure CORS to allow all methods and origins

--- src/main/java/dan/jms/dangateway/DanGatewayApplication.java
+++ src/main/java/dan/jms/dangateway/DanGatewayApplication.java
@@ -3,13 +3,24 @@ package dan.jms.dangateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableZuulProxy
 public class DanGatewayApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(DanGatewayApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry corsRegistry) {
+                corsRegistry.addMapping("/**").allowedMethods("*").allowedOrigins("*");
+            }
+        };
+    }
 }
